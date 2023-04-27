9918186adcac59e222dbaaa9fe6dd0aad872b71a
abdulbasit <habdulbasithassan77@gmail.com>
2023-04-27 14:46:55 +0300

    added footer

---
+++ phase-2-beta-project/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <p>
+        messi is the second greatest player in the world
+      </p>
+    </div>
+  )
+
+}
+
+export default Footer
